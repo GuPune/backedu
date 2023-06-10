fa299045ba846120f4e2b093eece05ae0aa66823
iboonkhet@gmail.com <123219570+Rkknoob1@users.noreply.github.com>
2023-06-10 11:57:21 +0700

    first C

---
+++ nuxt.config.ts
@@ -0,0 +1,38 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
+})
+
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+//   ssr: false,
+//   typescript: {
+//     shim: false
+//   },
+//   build: {
+//     transpile: ["vuetify"],
+//   },
+//   vite: {
+//     define: {
+//       "process.env.DEBUG": false,
+//     },
+//   },
+//   nitro: {
+//     serveStatic: true,
+//   },
+//   devServerHandlers: [],
+//   hooks: {
+//   },
+
+// })
+
+
