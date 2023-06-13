cdd1c393da92e4601aa9943cc9b9f281353a8fb4
iboonkhet <iboonkhet@gmail.com>
2023-06-13 21:43:08 +0700

    'auth' ok

--- store/auth.ts
+++ store/auth.ts
@@ -20,7 +20,7 @@ export const useAuthStore = defineStore('auth', {
   actions: {
     async authenticateUser({ username, password }: UserPayloadInterface) {
       // useFetch from nuxt 3
-      const { data, pending }: any = await useFetch('https://dummyjson.com/auth/login', {
+      const { error, data }: any = await useFetch('https://dummyjson.com/auth/login', {
         method: 'post',
         headers: { 'Content-Type': 'application/json' },
         body: {
@@ -28,14 +28,30 @@ export const useAuthStore = defineStore('auth', {
           password,
         },
       });
- 
+
+//       console.log(error.value.data);
+
+  //  alert(JSON.stringify(data.value));
+// // Might be interesting as well:
+// console.log(error.value.name, error.value.message);
+// alert(JSON.stringify(error.value.message));
+
   
+
   
       if (data.value) {
         const token = useCookie('token'); // useCookie new hook in nuxt 3
         token.value = data?.value?.token; // set token to cookie
         this.authenticated = true; // set authenticated  state value to true
       }
+      if (!data.value) {
+        this.loading = true;
+
+      }
+      // if(error.value.message){
+      //   alert('message');
+
+      // }
  
 
     },
@@ -49,3 +65,5 @@ export const useAuthStore = defineStore('auth', {
      },
   },
 });
+
+// {"__v_isShallow":false,"__v_isRef":true,"_rawValue":null,"_value":null}
